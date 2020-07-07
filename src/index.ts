39ecd79776a492038894721a6390b025e8ca8956
aelbore <aelbore.sg@gmail.com>
2020-07-07 09:50:54 +0800

    beta version that uses esbuild

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
-import forceSync from 'sync-rpc'
-import { join, resolve } from 'path'
+import * as path from 'path'
+import { buildSync } from 'esbuild'
+import { builtinModules } from 'module'
 
-const transform = forceSync(join(__dirname, 'esbuild'))
+const pkg = require(path.resolve('package.json'))
+
+const build = (filename: string) => {
+  const outFiles = buildSync({   
+    entryPoints: [ filename ],
+    outfile: './dist/main.js',
+    minify: false,
+    bundle: true, 
+    write: false,
+    target: 'esnext',
+    sourcemap: true,
+    external: [ 
+      ...builtinModules, 
+      ...Object.keys(pkg.dependencies ?? {}),
+      ...Object.keys(pkg.devDependencies ?? {}),
+      ...Object.keys(pkg.peerDependencies ?? {})
+    ]
+  })
+
+  return outFiles.outputFiles.reduce((cur, item) => {
+    const key = item.path.includes('.map') ? 'map': 'code'
+    cur[key] = Buffer.from(item.contents).toString()
+    return cur
+  }, {})
+}
 
 export default {
   process (content: string, filename: string) { 
-    const filePath = filename.replace(resolve(), '.')
-    return transform(filePath)
+    return build(filename)
   }  
 }
