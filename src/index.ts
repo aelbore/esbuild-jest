17400d9fa862fd63583b28419d26e862631049ef
aelbore <aelbore.sg@gmail.com>
2020-10-12 23:40:52 +0800

    add options to get tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
 import { transformSync } from 'esbuild'
-import path from 'path'
+import { extname, resolve } from 'path'
+import { readFileSync, existsSync } from 'fs'
+
+const getOptions = (): import('esbuild').TransformOptions => {
+  const tsconfigPath = resolve('tsconfig.json')
+  const tsconfig = existsSync(tsconfigPath) 
+    ? JSON.parse(readFileSync(resolve('tsconfig.json'), 'utf-8'))
+    : { compilerOptions: { target: 'es2018' } }
+
+   return {
+     format: 'cjs',
+     target: tsconfig.compilerOptions?.target || 'es2018',
+     sourcemap: true
+   }
+}
 
 export function process(content: string, filename: string) {
+  const options = getOptions()
+  
   const result = transformSync(content, {
-    loader: path.extname(filename).slice(1) as any,
-    format: 'cjs',
-    target: 'es2018',
-    sourcemap: true,
+    loader: extname(filename).slice(1) as any,
     sourcefile: filename,
+    ...options
   })
+
   return {
     code: result.js,
     map: {
       ...JSON.parse(result.jsSourceMap),
       sourcesContent: null,
-    },
+    }
   }
 }
