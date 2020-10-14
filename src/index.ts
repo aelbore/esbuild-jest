5f6c432096e1d2fd5d01065461124f8be6ebab5e
Tommaso De Rossi <beats.by.morse@gmail.com>
2020-10-14 17:28:54 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,15 @@ import { transformSync } from 'esbuild'
 import { extname, resolve } from 'path'
 import { readFileSync, existsSync } from 'fs'
 
-const getOptions = (): import('esbuild').TransformOptions => {
-  const tsconfigPath = resolve('tsconfig.json')
-  const tsconfig = existsSync(tsconfigPath) 
-    ? JSON.parse(readFileSync(resolve('tsconfig.json'), 'utf-8'))
-    : { compilerOptions: { target: 'es2018' } }
-
-   return {
-     format: 'cjs',
-     target: tsconfig.compilerOptions?.target || 'es2018',
-     sourcemap: true
-   }
-}
 
 export function process(content: string, filename: string) {
-  const options = getOptions()
   
   const result = transformSync(content, {
     loader: extname(filename).slice(1) as any,
     sourcefile: filename,
-    ...options
+    format: 'cjs',
+    target: 'es2018',
+    sourcemap: true,
   })
 
   return {
