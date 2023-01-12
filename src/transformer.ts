92b8c8f9f94a1513c2f16f4e83eb4975b5bf0aa5
Oleksii Novikov <oleksii@novikraft.com>
2023-01-13 00:58:35 +0200

    simplify the babel transformer

--- src/transformer.ts
+++ src/transformer.ts
@@ -1,24 +1,8 @@
-import { TransformOptions } from '@jest/transform'
-import { Config } from '@jest/types'
-
 import babelJest from 'babel-jest'
 
-const { process } = babelJest.createTransformer({
+export const { process: babelTransform } = babelJest.createTransformer({
   plugins: [ "@babel/plugin-transform-modules-commonjs" ],
   parserOpts: { 
     plugins: ["jsx", "typescript"],
   }
-})
-
-export interface BabelTransformOptions {
-  sourceText: string
-  sourcePath: string
-  config: Config.ProjectConfig
-  options?: TransformOptions
-}
-
-export function babelTransform(opts: BabelTransformOptions) {
-  const { sourceText, sourcePath, config, options } = opts
-  const babelResult = process(sourceText, sourcePath, config, options) as { code: string }
-  return babelResult.code
-}
+});
